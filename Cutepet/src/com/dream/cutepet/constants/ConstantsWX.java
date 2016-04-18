bc5e4119beaa9d86e026a993bc03c95e725747dd
Francis-Dreamer <534227772@qq.com>
2016-04-18 16:26:01 +0800

    新增 微信分享、GPS获取地址 功能

---
+++ Cutepet/src/com/dream/cutepet/constants/ConstantsWX.java
@@ -0,0 +1,12 @@
+package com.dream.cutepet.constants;
+
+public class ConstantsWX {
+	// APP_ID 替换为你的应用从官方网站申请到的合法appId
+	public static final String APP_ID = "wxebf6d6d0e9b45714";
+
+	public static class ShowMsgActivity {
+		public static final String STitle = "showmsg_title";
+		public static final String SMessage = "showmsg_message";
+		public static final String BAThumbData = "showmsg_thumb_data";
+	}
+}
